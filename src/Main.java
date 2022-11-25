8d684549d6f289a22d2b0bbc89a629e91d8d4839
sacitkaya <sacit.kaya@gmail.com>
2022-11-25 21:41:32 +0300

    Methodlar kullanılar aritmatik işlemler tanımlandı. Jar haline getirilerek yeni bir java projesinde kutuphaneye eklendi while loop yapısıyla kurgulanan menü yapısıyla entegre edildi

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,76 @@
 import java.util.Scanner;
+
+import static Islemler.Islemlistesi.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        char quit = 'n';
+        String input;
+        int choice = 0;
+
         Scanner scan = new Scanner(System.in);
+        while (quit != 'y') {
 
+            System.out.println("******************************* **********");
+            System.out.println("****** WELCOME TO MY CALCULATOR **********");
+            System.out.println("******************************************");
+            System.out.println("1. ADD          5. SIN        9. MATHPOWER");
+            System.out.println("2. SUBTRACT     6. COS       10. PERCENTAGE");
+            System.out.println("3. MULTIPLY     7. TAN       11. MOD");
+            System.out.println("4. DIVIDE       8. SQRT      12. ÇIKIŞ");
 
-        System.out.print("İşlem Yapılacak 1. Sayınızı Giriniz: ");
-        int sayi1 = scan.nextInt();
-        System.out.print("İşlem Yapılacak 2. Sayınzıı Giriniz: ");
-        int sayi2 = scan.nextInt();
-        System.out.println("****YAPILACAK İSLEMİNİZİ SEÇİNİZ***");
-        System.out.println("1. TOPLAMA");
-        System.out.println("2. ÇIKARMA");
-        System.out.println("3. ÇARPMA");
-        System.out.println("4. BÖLME");
-        System.out.print("Secimininizi giriniz: ");
-        int secim = scan.nextInt();
+            System.out.print("Please Choose a Function: ");
 
-        if (secim == 1) {
-            System.out.println("Toplama isleminizin sonucu = " + (sayi1 + sayi2));
-        } else if (secim == 2) {
-            System.out.println("Çıkarma isleminizin sonucu = " + (sayi1 - sayi2));
-        } else if (secim == 3) {
-            System.out.println("Carpma isleminizin sonucu = " + (sayi1 * sayi2));
-        } else if (secim == 4) {
-            System.out.println("Bölme isleminizin sonucu = " + (float) sayi1 / sayi2);
-        } else {
-            System.out.println("SEÇİMİNİZİ KONTROL EDİNİZ !!");
+            choice = scan.nextInt();
 
+            switch (choice) {
+                case 1:
+                    add();
+                    break;
+               case 2:
+                    subtract();
+                    break;
+                case 3:
+                    multiply();
+                    break;
+                case 4:
+                    divide();
+                    break;
+                case 5:
+                    sin();
+                    break;
+                case 6:
+                    cos();
+                    break;
+                case 7:
+                    tan();
+                    break;
+                case 8:
+                    sqrt();
+                case 9:
+                    mathpower();
+                    break;
+                case 10:
+                    percentage();
+                    break;
+                case 11:
+                    mod();
+                    break;
+                default:
+                    System.out.println("That wasn't a CHOICE ");
+            }
+            System.out.println("Would you like to QUIT y/n ?");
+            input = scan.next().toLowerCase();
+            quit = input.charAt(0);
         }
     }
 }
+
+
+
+
+
+
+
+
